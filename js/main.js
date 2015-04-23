46d3e36defedeb40306fc80101dc3b4855fa0355
Tom Benneche <tom@tjbenneche.com>
2015-04-23 10:15:57 -0700

    more layout work, placeholder icons and images, quote section

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var hh = $('header').outerHeight();
+
+$(window).scroll(function(){
+
+  if ($(window).pageYOffset() > hh - 50){
+    $('header').addClass('fixed');
+  }
+  else{
+    $('header').removeClass('fixed');
+  }
+
+});
+
+
+$('.nav-button').click(function(){
+  $('header nav ul').slideToggl();
+});
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
